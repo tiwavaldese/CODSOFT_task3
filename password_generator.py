5a2459adcda9d10771ec13b8642d76c16e64e15e
tiwavaldese <tiwaval7@gmail.com>
2024-01-14 19:38:12 +0100

    adding update file

--- password_generator.py
+++ password_generator.py
@@ -6,14 +6,17 @@ print("Hey! lets generate a password")
 
 #List of characters
 characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()abcdefghijklmnopqrstuvwxyz"
+
 #Prompt the user to specified the length of the password
 passwordLength = int(input("How long will you like your password to be ?"))
+
 #Generate a random password
 newPassword = []
 
 for x in range(passwordLength):
     #Append a random character to the newPassword
     newPassword.append(random.choice(characters))
+    
 #Join the list back to a string    
 finalPassword = ''.join(map(str, newPassword))
 
